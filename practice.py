6d68bc5b1b31796013e5a5edf10d3a47aefc4b4b
iqra <fhsaleem8104@gmail.com>
2024-07-24 12:20:07 +0500

    update

---
+++ practice.py
@@ -0,0 +1,12 @@
+# Problem Statement: Write a program to solve this age-related riddle! Anton, Beth, Chen, Drew, and Ethan are all friends. Their ages are as follows:
+# Anton is 21 years old.
+# Beth is 6 years older than Anton.
+# Chen is 20 years older than Beth.
+# Drew is as old as Chen's age plus Anton's age.
+# Ethan is the same age as Chen.
+# Your code should store each person's age to a variable and print their names and ages at the end
+anton_age : str = "21"
+beth_age : str  = "anthon_age + 6"
+chen_age : str = "beth_age + 20"
+drew_age : str = "chen+age + anthon_age"
+ethan_age : str = "chen_age"
